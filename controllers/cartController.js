94e29763f541ebf0891460739389580696b720a1
LuiosDyet <luisdartayet@gmail.com>
2021-11-10 17:34:22 -0300

    cart ejs done

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -1,6 +1,66 @@
+const cartProducts = [
+    {
+        id: 1,
+        nombre: "Fracciones I",
+        materia: "Matemática",
+        grado: "5to año",
+        profesor: {
+            nombre: "Juan",
+            apellido: "Rodriguez",
+        },
+        rating: 3.5,
+        precio: 450.0,
+        preview: "/img/cursos-preview/Fracciones5to.png",
+    },
+    {
+        id: 2,
+        nombre: "Sustantivos",
+        materia: "Práctica de lenguaje",
+        grado: "5to año",
+        profesor: {
+            nombre: "Juan",
+            apellido: "Perez",
+        },
+        rating: 4,
+        precio: 450.0,
+        preview: "/img/cursos-preview/Sustantivos5to.png",
+    },
+];
+const recommendations = [
+    {
+        id: 1,
+        nombre: "Fracciones I",
+        materia: "Matemática",
+        grado: "5to año",
+        profesor: {
+            nombre: "Juan",
+            apellido: "Rodriguez",
+        },
+        rating: 3.5,
+        precio: 450.0,
+        preview: "/img/cursos-preview/Fracciones5to.png",
+    },
+    {
+        id: 2,
+        nombre: "Sustantivos",
+        materia: "Práctica de lenguaje",
+        grado: "5to año",
+        profesor: {
+            nombre: "Juan",
+            apellido: "Perez",
+        },
+        rating: 4,
+        precio: 450.0,
+        preview: "/img/cursos-preview/Sustantivos5to.png",
+    },
+];
+
 const controller = {
     main: (req, res) => {
-        return res.render("cart.ejs");
+        return res.render("cart.ejs", {
+            enCarrito: cartProducts,
+            recommendations: recommendations,
+        });
     },
 };
 
