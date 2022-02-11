37ad64becf4bec36b424aaaf5fde360f9953642c
LuiosD <luisdartayet@gmail.com>
2022-02-11 06:30:23 -0300

    react installed

--- website/src/app.js
+++ website/src/app.js
@@ -1,33 +1,33 @@
-const path = require("path");
+const path = require('path');
 
-const express = require("express");
-const methodOverride = require("method-override");
-const session = require("express-session");
-const cookieParser = require("cookie-parser");
+const express = require('express');
+const methodOverride = require('method-override');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
 
-const userRoutes = require("./routes/userRoutes");
-const productRoutes = require("./routes/productRoutes");
-const homeRoute = require("./routes/homeRoute");
-const saleRoutes = require("./routes/saleRoutes");
+const userRoutes = require('./routes/userRoutes');
+const productRoutes = require('./routes/productRoutes');
+const homeRoute = require('./routes/homeRoute');
+const saleRoutes = require('./routes/saleRoutes');
 
 //Cómo hacer para pasar la función chosenBackground al middleware randomBackground? El problema es el app
-const randomBackground = require("./middleware/randomBackground");
-const userLogged = require("./middleware/userLogged");
-const sessionData = require("./middleware/session-locals");
+const randomBackground = require('./middleware/randomBackground');
+const userLogged = require('./middleware/userLogged');
+const sessionData = require('./middleware/session-locals');
 
 const app = express();
 
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
-app.use(express.static(path.join(__dirname, "../public")));
+app.use(express.static(path.join(__dirname, '../public')));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(methodOverride("_method"));
+app.use(methodOverride('_method'));
 app.use(cookieParser());
 app.use(
     session({
-        secret: "Shh, esto es secreto",
+        secret: 'Shh, esto es secreto',
         resave: false,
         saveUninitialized: false,
     })
@@ -38,13 +38,13 @@ app.use(randomBackground());
 app.use(userLogged);
 app.use(sessionData);
 
-app.listen(3000);
+app.listen(3001);
 
-app.use("/", homeRoute);
-app.use("/user", userRoutes);
-app.use("/products", productRoutes);
-app.use("/sale", saleRoutes);
+app.use('/', homeRoute);
+app.use('/user', userRoutes);
+app.use('/products', productRoutes);
+app.use('/sale', saleRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).render("not-found");
+    res.status(404).render('not-found');
 });
