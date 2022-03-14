2324adb09addcbb613682d8a9d717b0c5f4d5c82
LuiosD <luisdartayet@gmail.com>
2022-03-14 15:36:44 -0300

    updated production db

--- website/src/database/config/config.js
+++ website/src/database/config/config.js
@@ -15,10 +15,11 @@ module.exports = {
         dialect: 'mysql',
     },
     production: {
-        username: 'root',
-        password: null,
-        database: 'database_production',
-        host: '127.0.0.1',
+        username: 'ba8fd7d5c50cf8',
+        password: 'd9dbcbde',
+        database: 'heroku_ee8c2601b233ff8',
+        host: 'us-cdbr-east-05.cleardb.net',
         dialect: 'mysql',
+        logging: false,
     },
 };
